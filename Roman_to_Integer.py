e14e245a4ff7869714a21b96645f18091b9b5c7e
Vaishali-Bhosle <122103712+Vaishali-Bhosle@users.noreply.github.com>
2023-10-10 04:08:40 +0530

    Create Roman_to_Integer.py

---
+++ Roman_to_Integer.py
@@ -0,0 +1,25 @@
+def R2I(rn):
+    rdict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,4b.py
+        'M': 1000
+    }
+    result = 0
+    prev = 0
+    for i in rn[::-1]:
+        value = rdict[i]
+        if value > prev:
+            result += value
+        else:
+            result -= value
+        prev = value
+    return result
+
+
+print("Conversion")
+a = input("Enter a number")
+print("The Decimal Equivalent is ", R2I(a))
