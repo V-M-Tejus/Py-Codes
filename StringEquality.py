822f15975ee04b618c7c4791b9778997a4f53fed
Yeddulapall-Sahithi <145190139+Yeddulapall-Sahithi@users.noreply.github.com>
2023-10-30 15:21:05 +0530

    Create StringEquality.py

---
+++ StringEquality.py
@@ -0,0 +1,12 @@
+s1 = 'Apple'
+
+s2 = 'Apple'
+
+s3 = 'apple'
+
+# case sensitive equals check
+if s1 == s2:
+    print('s1 and s2 are equal.')
+
+if s1.__eq__(s2):
+    print('s1 and s2 are equal.')
